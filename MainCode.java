a530eb4c9f778e0508876c8e1ba7cc6fabda277a
MichaelT4646 <mtambascio@quinnipiac.edu>
2021-02-20 14:50:10 -0500

    Added a scanner and a if statement

--- MainCode.java
+++ MainCode.java
@@ -1,7 +1,13 @@
-
+import java.util.Scanner;
 public class MainCode {
 	public static void main(String[] args) {
+		Scanner keyboardInput = new Scanner(System.in);
+		
 		System.out.println("Hello! Want to play the game?");
 		System.out.println("How are you?");
+		String userInput = keyboardInput.nextLine();
+		if(userInput == "good".toLowerCase()) {
+			System.out.println("I'm glad you feel that way!");
+		}
 	}
 }
