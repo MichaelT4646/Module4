d38d17e30c882081f4556943c292387eafb5f734
MichaelT4646 <mtambascio@quinnipiac.edu>
2021-02-23 14:06:57 -0500

    Fixing the issue pointed out by Isaac

--- MainCode.java
+++ MainCode.java
@@ -7,13 +7,14 @@ public class MainCode {
 		System.out.println("Hello! Want to play the game?");
 		System.out.println("How are you?");
 		String userInput = keyboardInput.nextLine();
-		if (userInput == "good".toLowerCase()) {
+		userInput = userInput.toLowerCase();
+		if (userInput == "good") {
 			System.out.println("I'm glad you feel that way!");
 		}
 		else{
 			System.out.println("That's a cool way to feel I guess...");
 		}
 		
-		System.out.println("Well thanks for telling me how you feel!");
+		System.out.println("Well, thanks for telling me how you feel!");
 	}
 }
